fc74f31f5f228abc254eaed54c2e6a4dea8dbff3
Yahir Flores <77yahir80@gmail.com>
2026-01-14 00:38:26 -0800

    Initial Commit

---
+++ src/main/java/com/yahir/ratelimitedapi/RateLimitedApiApplication.java
@@ -0,0 +1,13 @@
+package com.yahir.ratelimitedapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimitedApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateLimitedApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yahir/ratelimitedapi/RateLimitedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yahir.ratelimitedapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimitedApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
